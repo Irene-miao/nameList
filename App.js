93cc9b1a850b0365ffd2e07af5ae5b8fed248e16
Irene <irene_lee@live.com.sg>
2020-12-07 10:26:51 +0800

    First commit

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const names = ['John', 'Bob', 'May', 'Jane', 'Tina'];
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{names[0]}</Text>
+      <Text>{names[1]}</Text>
+      <Text>{names[2]}</Text>
+      <Text>{names[3]}</Text>
+      <Text>{names[4]}</Text>
+     
+     
       <StatusBar style="auto" />
     </View>
   );
